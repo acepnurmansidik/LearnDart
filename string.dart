4d437587d11bfe4f361fc09f03bc8ffd4f58b00a
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-27 19:04:41 +0700

    String

---
+++ string.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = ' acep nurman sidik ';
+  // cek apakah namanya mengandung string tertentu
+  print(name.contains('nurman'));
+  // mencetak mula dari sampai ke
+  print(name.substring(6, 12));
+  //menghilanhka space dari awala dan belakang
+  print(name.trim());
+  // mengilangkan space akhir
+  print(name.trimRight());
+}
