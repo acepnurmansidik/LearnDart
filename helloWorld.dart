83132576d40fc828632cabd00ca7ab487859228e
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-27 17:33:11 +0700

    E1-5

---
+++ helloWorld.dart
@@ -0,0 +1,7 @@
+var persons = {'name': 'acep', 'age': 23, 'hobbies': 'coding, watch anime'};
+double asd = 3.5;
+String asd1 = 'asd';
+
+void main() {
+  print('Name: ${persons['name']} ${asd1}');
+}
