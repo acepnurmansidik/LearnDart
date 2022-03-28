71c8459f9435f660cbd6d8791dc0d77cc2602ffe
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-28 08:00:11 +0700

    List/array

---
+++ listType.dart
@@ -0,0 +1,13 @@
+void main() {
+  // klasifikasi string
+  // List<String> collages = ['acep','nurman'];
+
+  //
+  List collages = ['acep', 'nurman'];
+
+  print(collages);
+  // menambahkan list dengan list
+  List<String> collages1 = ['sidik'];
+  collages.addAll(collages1);
+  print(collages);
+}
