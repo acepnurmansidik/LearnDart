da352b5825e8c75945e45c190fcd73a81e0ecd04
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-28 09:45:07 +0700

    Operators

---
+++ operators.dart
@@ -0,0 +1,33 @@
+void main() {
+  var a = 10;
+  var b = 4;
+  // ### Aritmatika
+  // penjumlahan
+  var penjumlahan = a + b;
+  // pengurangan
+  var pengurangan = a - b;
+  // perkalian
+  var perkalian = a * b;
+  // pembagian
+  var pembagian = a / b;
+  // modulus
+  var modulus = a % b;
+
+  // ### Equality & relational operators
+  print('=== Equality & relational operators');
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+  print(a != b);
+  print(a == b);
+
+  // Logical operator
+  print('=== Logical operator');
+  var x = true;
+  var y = false;
+
+  print(x && y);
+  print(x || y);
+  print(!x);
+}
