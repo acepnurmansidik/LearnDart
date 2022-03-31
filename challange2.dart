fa98727fc06cb01a8325006b5f8a7595ab76b12d
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-31 13:33:53 +0700

    challange 2

---
+++ challange2.dart
@@ -0,0 +1,18 @@
+void main() {
+  int sisi = 9;
+  var Kel_persegi = sisi * 4;
+  print(Kel_persegi);
+
+  // Luas Lingkaran
+  double phi = 3.14;
+  int r2 = 8;
+  var Luas_Lingkaran = phi * (r2 * r2);
+  print('${Luas_Lingkaran} cm2');
+
+  // volume balok
+  int pangjang = 3;
+  int lebar = 3;
+  int tinggi = 3;
+  var VB = pangjang * lebar * tinggi;
+  print(VB);
+}
