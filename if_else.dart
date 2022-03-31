f181a89e1a3ad85281e00851903d02e81fb9396a
acepnurmansidik <acepnurmansidik@gmail.com>
2022-03-31 19:00:20 +0700

    Flow control if else

---
+++ if_else.dart
@@ -0,0 +1,10 @@
+void main() {
+  int angka = 5;
+  // if (angka % 2 == 0) {
+  //   print('Genap');
+  // } else {
+  //   print('Ganjil');
+  // }
+
+  angka % 2 == 0 ? print('Genap') : print('Ganjil');
+}
