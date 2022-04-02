dee1a6378f8e3bc1953572091c13f79b23953bd6
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-03 03:23:33 +0700

    class constructor

--- class.dart
+++ class.dart
@@ -1,17 +1,30 @@
 void main() {
-  RekeningBank myrekening = new RekeningBank();
-  myrekening.cekSaldo();
-  // =====
-  myrekening.namaBank = 'MANDIRI';
-  myrekening.namaPemilik = 'Acep Nurman Sidik';
-  myrekening.saldo = 30000000000;
-  myrekening.cekSaldo();
+  // RekeningBank myrekening = new RekeningBank();
+  // myrekening.cekSaldo();
+  // // =====
+  // myrekening.namaBank = 'MANDIRI';
+  // myrekening.namaPemilik = 'Acep Nurman Sidik';
+  // myrekening.saldo = 30000000000;
+  // myrekening.cekSaldo();
+
+  // constructor
+  RekeningBank rekeningAcep = new RekeningBank(
+      namaPemilik: "acep",
+      namaBank: "BCA",
+      saldo: 50000000,
+      email: 'acepnurmansidik');
+  rekeningAcep.cekSaldo();
+  rekeningAcep.aboutMe();
 }
 
 class RekeningBank {
-  String namaPemilik = '';
-  String namaBank = '';
-  int saldo = 0;
+  String? namaPemilik;
+  String? namaBank;
+  int? saldo;
+  final email;
+
+// constructor
+  RekeningBank({this.namaPemilik, this.namaBank, this.saldo, this.email});
 
   cekSaldo() {
     print('Saldo anda saat ini: Rp.${saldo}');
@@ -23,5 +36,9 @@ class RekeningBank {
 
   ambilSaldo() {
     print('Ambil Saldo');
+  }
+
+  aboutMe() {
+    print('Your email: $email');
   }
 }
