29284c4302fe53fa504e5f09066c81853a4ad7b7
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-03 16:59:18 +0700

    Setter & getters

--- class.dart
+++ class.dart
@@ -1,20 +1,30 @@
 void main() {
-  // RekeningBank myrekening = new RekeningBank();
-  // myrekening.cekSaldo();
-  // // =====
-  // myrekening.namaBank = 'MANDIRI';
-  // myrekening.namaPemilik = 'Acep Nurman Sidik';
-  // myrekening.saldo = 30000000000;
-  // myrekening.cekSaldo();
+  RekeningBank myrekening = new RekeningBank();
+  myrekening.cekSaldo();
+  // =====
+  myrekening.namaBank = 'MANDIRI';
+  myrekening.namaPemilik = 'Acep Nurman Sidik';
+  myrekening.saldo = 30000000000;
+  myrekening.cekSaldo();
+  print("==============================");
 
   // constructor
   RekeningBank rekeningAcep = new RekeningBank(
       namaPemilik: "acep",
       namaBank: "BCA",
       saldo: 50000000,
-      email: 'acepnurmansidik');
+      email: 'acepnurmansidik@gmail.com');
   rekeningAcep.cekSaldo();
   rekeningAcep.aboutMe();
+  print("==============================");
+
+  RekeningBank rekeningDian =
+      new RekeningBank(namaBank: 'BTN', namaPemilik: 'Dian', saldo: 5000);
+  rekeningDian.setSaldo = 50000;
+  print(rekeningDian.getSaldo);
+  print(rekeningDian.namaBank);
+  rekeningDian.setBankName = 'BRO';
+  print(rekeningDian.namaBank);
 }
 
 class RekeningBank {
@@ -22,6 +32,32 @@ class RekeningBank {
   String? namaBank;
   int? saldo;
   final email;
+
+  // setters
+  set setSaldo(int saldo) {
+    this.saldo = saldo;
+  }
+
+  set setOwner(String owner) {
+    this.namaPemilik = owner;
+  }
+
+  set setBankName(String bank) {
+    this.namaBank = bank;
+  }
+
+  // getters
+  int? get getSaldo {
+    return saldo;
+  }
+
+  String? get getOwner {
+    return namaPemilik;
+  }
+
+  String? get getBankName {
+    return namaBank;
+  }
 
 // constructor
   RekeningBank({this.namaPemilik, this.namaBank, this.saldo, this.email});
