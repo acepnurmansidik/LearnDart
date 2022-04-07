21544f5c5f0ceea67330fff9ba0b1fdc09cc2efd
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-07 07:42:49 +0700

    try catch

--- syn&asyn.dart
+++ syn&asyn.dart
@@ -1,6 +1,7 @@
-Future<void> main() async{
+Future<void> main() async {
   print(await printData());
   print('Data has been obtained');
+  print(await printDataErr());
 }
 
 Future<String> printData() async {
@@ -10,4 +11,18 @@ Future<String> printData() async {
 
 Future<String> fetchData() {
   return Future.delayed(Duration(seconds: 5), () => 'fetch data');
+}
+
+// trycatch
+Future<Object> printDataErr() async {
+  try {
+    var data = await fetchDataERR();
+    return 'data: $data';
+  } catch (err) {
+    return err;
+  }
+}
+
+Future<String> fetchDataERR() {
+  return Future.delayed(Duration(seconds: 5), () => throw ('ERROR'));
 }
